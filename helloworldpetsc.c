cd823af17055437d73e4a837711d648a0369b68e
Unknown <chencheng502@gmail.com>
2017-04-03 00:01:01 -0400

    compile error

--- helloworldpetsc.c
+++ helloworldpetsc.c
@@ -40,7 +40,7 @@ int main(int argc, char** argv)
 
   */
 
-	ierr = PetscSynchronizePrintf(PETSC_COMM_WORLD, "Synchronized Hello from %d\n", rank); CHKERRQ(ierr);
+	ierr = PetscSynchronizedPrintf(PETSC_COMM_WORLD, "Synchronized Hello from %d\n", rank); CHKERRQ(ierr);
   /*Prints synchronized output from several processors. Output of the first processor is followed by that of the second, etc*/
 
 	ierr = PetscSynchronizedFlush(PETSC_COMM_WORLD,PETSC_STDOUT); CHKERRQ(ierr);
